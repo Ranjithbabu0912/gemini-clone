9641d987693bfe7d3beb8900c43136234fdfe3c9
Ranjith Babu <ranjithbabu0912@gmail.com>
2024-09-01 14:18:17 +0530

    first commit

---
+++ src/assets/assets.js
@@ -0,0 +1,41 @@
+import bulb_icon from './bulb.png'
+import code_icon from './code.png'
+import compass_icon from './compass.png'
+import gallery_icon from './gallery.png'
+import gallery2_icon from './gallery2.png' 
+import gemini_icon from './gemini-icon.png'
+import question_icon from './help.png'
+import history_icon from './history.png'
+import menu_icon from './menu.png'
+import messege_icon from './messege.png'
+import mic_icon from './mic.png'
+import plus_icon from './plus.png'
+import send_icon from './send.png'
+import settings_icon from './settings.png'
+import user_icon from './user.png'
+import youtube_icon from './youtube.png'
+import copy_icon from './copy.png'
+
+
+
+
+export const assets = {
+    bulb_icon,
+    code_icon,
+    compass_icon,
+    gallery_icon,
+    gallery2_icon,
+    gemini_icon,
+    question_icon,
+    history_icon,
+    menu_icon,
+    messege_icon,
+    mic_icon,
+    plus_icon,
+    send_icon,
+    settings_icon,
+    user_icon,
+    youtube_icon,
+    copy_icon
+}
+
